53b1daa937ffc61965d37c9048bf0b8405f3c660
[scalix] <git@scalix.world>
2026-01-07 16:51:14 +0530

    [scalix] Added Material UI dependencies and ThemeProvider. - wrote 3 file(s), added @mui/material, @emotion/react, @emotion/styled package(s)

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,27 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom"; // Import BrowserRouter
+import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider, createTheme } from '@mui/material/styles'; // Import ThemeProvider and createTheme
+
+// Define your Material UI theme
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#1976d2', // Example primary color
+    },
+    secondary: {
+      main: '#dc0041', // Example secondary color
+    },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter> {/* Wrap App with BrowserRouter */}
-      <App />
+    <BrowserRouter>
+      <ThemeProvider theme={theme}> {/* Wrap App with ThemeProvider */}
+        <App />
+      </ThemeProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
